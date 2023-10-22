a21ae01ccc7d72e720e871196cf06a26d26df726
Your Name <you@example.com>
2023-10-22 09:09:41 +0300

    Removed only comments

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const rl = readline.createInterface({
 })
 
 
-// console.log(getSalesByDay(data, 5220, "2023-01-01", "2023-01-30", 1));
 console.log();
 console.log(chalk.black.bgRed.bold("============================= this is Daily sales calc ============================="));
 console.log();
@@ -18,12 +17,7 @@ console.log();
 console.log();
 
     rl.question("Enter second month :- ",(secondMonth)=>{
-      // const filtered = data.filter((item)=>item.id >= 1 && item.id <= 12)
-      // if(!filtered){
-      //   console.log(getSalesByDay(data, 5220, firstMonth, secondMonth, 1));
-      // }else{
-      //   console.log(chalk.bgMagentaBright("no such date is availibe.. "))
-      // }
+     
       console.log(getSalesByDay(data, 5220, firstMonth, secondMonth, 1));
       process.exit(1);
     });
